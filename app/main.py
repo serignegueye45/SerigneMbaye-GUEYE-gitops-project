5b4e7a6be875fab10c70ea735b98945d05702622
Serigne Mbaye GUEYE <smggueye45@gmail.com>
2026-07-22 19:44:55 +0000

    Personalize app with student name

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Prénom Nom - Projet GitOps", "status": "ok"}
+    return {"message": "Serigne Mbaye Gueye - Projet GitOps", "status": "ok"}
 
 @app.route("/api/whoami")
 def whoami():
-    return {"student": "prenom-nom"}
+    return {"student": "serigne-mbaye-gueye"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
